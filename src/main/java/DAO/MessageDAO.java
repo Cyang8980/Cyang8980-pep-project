57e041e0e731c63f6cd4724f81cbf0af20c15530
Calvin Yang <cyang@skidmore.edu>
2023-08-29 18:28:49 +0000

    passed create and retrieve message test

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -42,22 +42,13 @@ public class MessageDAO {
      */
     public void deleteMessageByMessageID(int message_id) {
         Connection connection = ConnectionUtil.getConnection();
-        List<Message> messages = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "DELETE FROM message WHERE message_id = (?)";
+            String sql = "DELETE FROM message WHERE message_id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, message_id);
-            ResultSet rs = preparedStatement.executeQuery();
-            while(rs.next()){
-                Message message = new Message(
-                        rs.getInt("message_id"),
-                        rs.getInt("posted_by"),
-                        rs.getString("message_text"),
-                        rs.getLong("time_posted_epoch")
-                        );
-                messages.remove(message);
-            }
+            preparedStatement.executeUpdate();
+            
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
@@ -71,18 +62,25 @@ public class MessageDAO {
         try {
             //Write SQL logic here. You should only be inserting with the name column, so that the database may
             //automatically generate a primary key.
-            String sql = "INSERT INTO message (posted_by) (message_text) (time_posted_epoch) VALUES (?) (?) (?)";
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            
-            //write preparedStatement's setString method here.
+
+            // Write preparedStatement's setXXX methods here.
             preparedStatement.setInt(1, message.getPosted_by());
             preparedStatement.setString(2, message.getMessage_text());
             preparedStatement.setLong(3, message.getTime_posted_epoch());
-            preparedStatement.executeUpdate();
-            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
-            if(pkeyResultSet.next()){
-                int generated_message_id = (int) pkeyResultSet.getLong(1);
-                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                // makes sure message isnt blank or too big
+                if (message.getMessage_text() != "" && message.getMessage_text().length() < 254) {
+                    ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+                    if (pkeyResultSet.next()) {
+                        int generated_message_id = pkeyResultSet.getInt(1);
+                        return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+                    }
+                }
             }
         }catch(SQLException e){
             System.out.println(e.getMessage());
